96d87f9076cb8657972751411827ba5364a44b9a
Arthelon <hsing.daniel@gmail.com>
2016-08-15 08:28:09 +0800

    Altered datastore directory

--- app/config/database.js
+++ app/config/database.js
@@ -2,8 +2,24 @@ import low from 'lowdb'
 import os from 'os'
 import path from 'path'
 import underscore from 'underscore-db'
+import fs from 'fs'
 
-const db = low(path.join(os.homedir(), 'contactStories.json'))
+const base_path = path.join(os.homedir(), '.contactStories')
+const db_path = path.join(base_path, 'contactStories.json')
+
+fs.stat(base_path, (err, stats) => {
+	if (err || !stats.isDirectory()) {
+		fs.mkdir(base_path)
+	}
+})
+
+fs.stat(db_path, (err, stats) => {
+	if (err || !stats.isFile()) {
+		fs.writeFile(db_path, "")
+	}
+})
+
+const db = low(path.join(base_path, 'contactStories.json'))
 db._.mixin(underscore)
 
 export default db
