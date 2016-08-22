63856d60856788f66e922c357939c93b67b3cd7a
Arthelon <hsing.daniel@gmail.com>
2016-08-22 17:33:53 +0800

    Updated story component with material-ui components and additional props

--- app/components/story/Story.js
+++ app/components/story/Story.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'
+import { Card, CardActions, CardHeader, CardText} from 'material-ui/Card'
+import FlatButton from 'material-ui/FlatButton'
 
 export default class Story extends Component {
 
     static propTypes = {
-        text: React.PropTypes.string.isRequired
+        title: React.PropTypes.string,
+        text: React.PropTypes.string.isRequired,
+        handleDelete: React.PropTypes.func
     }
 
     render() {
         return (
-            <div>
-                <p>{this.props.text}</p>
-            </div>
+            <Card key={this.props.id}>
+                <CardHeader
+                    title={this.props.title}
+                    actAsExpander={true}
+                    showExpandableButton={true}
+                    />
+                <CardText expandable={true}>
+                    {this.props.text}
+                </CardText>
+                <CardActions>
+                    <FlatButton onClick={this.props.handleDelete.bind(this)}>Delete</FlatButton>
+                </CardActions>
+            </Card>
         )
     }
 }
